1c8a5913ce5ee650c36422b60fdb5c87e86c65f2
Federico Salgado <federicosalgado2002@gmail.com>
2025-01-14 22:30:44 -0300

    Update repository to be offline first - sort cities alphabetically per country

--- app/src/main/java/com/example/composecitylist/data/CityRepository.kt
+++ app/src/main/java/com/example/composecitylist/data/CityRepository.kt
@@ -2,17 +2,25 @@ package com.example.composecitylist.data
 
 import com.example.composecitylist.domain.City
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class CityRepository(
     private val remoteDataSource: CityDataSource,
     private val localDataSource: CityDataSource
 ) {
-    suspend fun getCities(): Flow<List<City>> {
-        return localDataSource.getCities().onCompletion {
-            val remoteCities = remoteDataSource.getCities().first()
-            localDataSource.updateCities(remoteCities)
+    fun getCities(): Flow<List<City>> {
+        return flow {
+            val localCities = localDataSource.getCities()
+            if (localCities.first().isEmpty()) {
+                val remoteCities = remoteDataSource.getCities()
+                localDataSource.updateCities(remoteCities.first().sortedBy { it.countryCode })
+                emitAll(remoteCities)
+            } else {
+                emitAll(localCities.map { it.sortedBy { city -> city.countryCode } })
+            }
         }
     }
 
