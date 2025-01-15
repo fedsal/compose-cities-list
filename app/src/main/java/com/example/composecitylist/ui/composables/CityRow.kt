bb092bff859d9202ab63b02917c19242a3d33937
Federico Salgado <federicosalgado2002@gmail.com>
2025-01-15 11:12:36 -0300

    Show loading indicator in city row when fav button is pressed

--- app/src/main/java/com/example/composecitylist/ui/composables/CityRow.kt
+++ app/src/main/java/com/example/composecitylist/ui/composables/CityRow.kt
@@ -18,10 +18,14 @@ import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,6 +33,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composecitylist.domain.City
+import kotlinx.coroutines.delay
 
 @Composable
 fun CityRow(city: City, onFavouriteClick: () -> Unit = {}, onClick: (city: City) -> Unit = {}) {
@@ -59,17 +64,35 @@ fun CityRow(city: City, onFavouriteClick: () -> Unit = {}, onClick: (city: City)
                         fontSize = 12.sp
                     )
                 }
+                val showLoading = remember { mutableStateOf(false) }
                 Button(
-                    onClick = { onFavouriteClick() },
+                    onClick = {
+                        if (!showLoading.value) {
+                            onFavouriteClick()
+                            showLoading.value = true
+                        }
+                    },
                     modifier = Modifier.fillMaxHeight(),
                     colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
                 ) {
-                    Icon(
-                        imageVector = if (city.isFavourite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-                        contentDescription = "Favourite Icon",
-                        tint = MaterialTheme.colorScheme.primary
-                    )
+                    if (showLoading.value) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(24.dp),
+                            strokeWidth = 2.dp
+                        )
+                        LaunchedEffect(Unit) {
+                            delay(1000L)
+                            showLoading.value = false
+                        }
+                    } else {
+                        Icon(
+                            imageVector = if (city.isFavourite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                            contentDescription = "Favourite Icon",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
                 }
+
             }
         }
     }
